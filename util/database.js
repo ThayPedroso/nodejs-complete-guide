5f570c3ea991073500825029191e44b73cb25d89
ThayPedroso <44008476+ThayPedroso@users.noreply.github.com>
2020-03-23 09:31:54 -0300

    UsingSequelize
    Using sequelize to write and read from database. Also to create relations between tables.

--- util/database.js
+++ util/database.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
-const mysql = require('mysql2')
+const Sequelize = require('sequelize')
 
-const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    database: process.env.MYSQL_DATABASE,
-    password: process.env.MYSQL_PASS,
-})
+const sequelize = new Sequelize(
+    process.env.MYSQL_DATABASE,
+    process.env.MYSQL_USER,
+    process.env.MYSQL_PASS,
+    {
+        dialect: 'mysql',
+        host: process.env.MYSQL_HOST,
+    }
+)
 
-module.exports = pool.promise()
+module.exports = sequelize
