795ad35186dfcf549112a49962a09d569ba501f8
ThayPedroso <thayla.pedroso88@gmail.com>
2020-06-09 22:50:35 -0300

    mongoPart1
    mongodb connection part1

--- app.js
+++ app.js
@@ -4,13 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error')
-const sequelize = require('./util/database')
-const Product = require('./models/product')
-const User = require('./models/user')
-const Cart = require('./models/cart')
-const CartItem = require('./models/cart-item')
-const Order = require('./models/order')
-const OrderItem = require('./models/order-item')
+const mongoConnect = require('./util/database').mongoConnect
 
 const app = express();
 
@@ -18,58 +12,27 @@ app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findByPk(1)
-        .then(user => {
-            req.user = user
-            next()
-        })
-        .catch(err => console.log(err))
+    // User.findByPk(1)
+    //     .then(user => {
+    //         req.user = user
+    //         next()
+    //     })
+    //     .catch(err => console.log(err))
+    next()
 })
 
 app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// Relations between tables
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
-User.hasMany(Product)
-User.hasOne(Cart)
-Cart.belongsTo(User)
-Cart.belongsToMany(Product, { through: CartItem })
-Product.belongsToMany(Cart, { through: CartItem })
-Order.belongsTo(User)
-User.hasMany(Order)
-Order.belongsToMany(Product, { through: OrderItem })
-
-// sync() will syncronize to the database configured in the models 
-// with define() and create tables if necessary
-sequelize
-    .sync()
-    // .sync({ force: true }) // force to ensure tables will be recreated during development
-    .then(result => {
-        return User.findByPk(1)
-    })
-    .then(user => {
-        if(!user) {
-            return User.create({ name: 'Thayla', email: 'test@test.com' })
-        }
-        return Promise.resolve(user)
-    })
-    .then(user => {
-        // console.log(user)
-        user.createCart()
-    })
-    .then(cart => {
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err)
-    })
+mongoConnect(() => {
+    app.listen(3000)
+})
 
--- util/database.js
+++ util/database.js
@@ -1,14 +1,33 @@
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 require('dotenv').config()
-const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(
-    process.env.MYSQL_DATABASE,
-    process.env.MYSQL_USER,
-    process.env.MYSQL_PASS,
-    {
-        dialect: 'mysql',
-        host: process.env.MYSQL_HOST,
+const user = process.env.DB_USER
+const password = process.env.DB_PASSWORD
+const name = process.env.DB_NAME
+
+let db
+
+const mongoConnect = callback => {
+
+    MongoClient.connect(`mongodb+srv://${user}:${password}@cluster0-xk1im.mongodb.net/${name}?retryWrites=true&w=majority`)
+    .then(client => {
+        console.log('Connected!')
+        db = client.db()
+        callback()
+    })
+    .catch(err => {
+        console.log(err)
+        throw err
+    })
+}
+
+const getDb = () => {
+    if(db) {
+        return db
     }
-)
+    throw "No database found!"
+}
 
-module.exports = sequelize
+exports.mongoConnect = mongoConnect
+exports.getDb = getDb
